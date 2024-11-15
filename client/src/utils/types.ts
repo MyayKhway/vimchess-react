867a37bb86fba5a6f41abff69c7049e0ca5f6d96
KentNyiLynn <kentnyilynn@proton.me>
2024-11-15 14:21:58 +0700

    fix piece moved event

--- client/src/utils/types.ts
+++ client/src/utils/types.ts
@@ -1,64 +1,63 @@
 export type SquareType = {
-    index: number,
-    piece: string | null,
+  index: number,
+  piece: string | null,
 }
 
 export type BoardType = [
-    [SquareType, SquareType, SquareType, SquareType, SquareType, SquareType, SquareType, SquareType], 
-    [SquareType, SquareType, SquareType, SquareType, SquareType, SquareType, SquareType, SquareType], 
-    [SquareType, SquareType, SquareType, SquareType, SquareType, SquareType, SquareType, SquareType], 
-    [SquareType, SquareType, SquareType, SquareType, SquareType, SquareType, SquareType, SquareType], 
-    [SquareType, SquareType, SquareType, SquareType, SquareType, SquareType, SquareType, SquareType], 
-    [SquareType, SquareType, SquareType, SquareType, SquareType, SquareType, SquareType, SquareType], 
-    [SquareType, SquareType, SquareType, SquareType, SquareType, SquareType, SquareType, SquareType], 
-    [SquareType, SquareType, SquareType, SquareType, SquareType, SquareType, SquareType, SquareType]
+  [SquareType, SquareType, SquareType, SquareType, SquareType, SquareType, SquareType, SquareType],
+  [SquareType, SquareType, SquareType, SquareType, SquareType, SquareType, SquareType, SquareType],
+  [SquareType, SquareType, SquareType, SquareType, SquareType, SquareType, SquareType, SquareType],
+  [SquareType, SquareType, SquareType, SquareType, SquareType, SquareType, SquareType, SquareType],
+  [SquareType, SquareType, SquareType, SquareType, SquareType, SquareType, SquareType, SquareType],
+  [SquareType, SquareType, SquareType, SquareType, SquareType, SquareType, SquareType, SquareType],
+  [SquareType, SquareType, SquareType, SquareType, SquareType, SquareType, SquareType, SquareType],
+  [SquareType, SquareType, SquareType, SquareType, SquareType, SquareType, SquareType, SquareType]
 ];
 
 export interface GameType {
-    white: string,
-    black: string,
-    board: BoardType,
-    graveyard: GraveyardType
+  white: string,
+  black: string,
+  board: BoardType,
+  graveyard: GraveyardType
 }
 
 export interface GamesType {
-    [key: string]: GameType
+  [key: string]: GameType
 }
 
 export interface GraveyardType {
-    white: string[],
-    black: string[]
+  white: string[],
+  black: string[]
 }
 
 export interface ServerToClientEvents {
-    'game ready': (game: GameType, game_code: string) => void,
-    'Defeat': () => void,
-    'Victory': () => void,
-    'board update': (game: GameType) => void,
-    'game not created': (id: string) => void,
+  'game ready': (game: GameType, game_code: string) => void,
+  'Defeat': () => void,
+  'Victory': () => void,
+  'board update': (game: GameType) => void,
+  'game not created': (id: string) => void,
 
 }
 
 export interface ClientToServerEvents {
-    'game create': (sock_id: string) => void,
-    'piece captured': (board: BoardType,
-        sock_id: string,
-        white_piece: string,
-        black_piece: string,
-        game_code: string
-    ) => void,
-    'piece moved': (
-        board: BoardType,
-        sock_id: string,
-        game_code: string
-    ) => void,
-    'game join': (
-        sock_id: string,
-        game_code: string,
-    ) => void,
-    'dummy': (
-        board: BoardType
-    ) => void,
+  'game create': (sock_id: string) => void,
+  'piece captured': (board: BoardType,
+    sock_id: string,
+    white_piece: string,
+    black_piece: string,
+    game_code: string
+  ) => void,
+  'piece moved': (
+    board: BoardType,
+    game_code: string
+  ) => void,
+  'game join': (
+    sock_id: string,
+    game_code: string,
+  ) => void,
+  'dummy': (
+    board: BoardType
+  ) => void,
 }
 
 export interface SocketData {
